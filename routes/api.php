9c0c950ad39d738d63479c49d0559cd4d1f7f603
martin conde <martincon888@gmail.com>
2026-01-29 22:30:09 -0600

    - se agrego la ruta GET /v1/Explorer_courses - se creo CourseController y ExplorerCourseService - se actualizoz el modelo Course con la PK cou_token y la relación con servicios

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CourseController;
 
 Route::post('/v1/auth/login', [UserController::class, 'login'])->middleware('auth:sanctum');
 Route::post('/v1/auth/register', [UserController::class, 'register'])->middleware('auth:sanctum');
 
 Route::post('/v1/email/verification', [UserController::class, 'sendVerificationEmail'])->middleware('auth:sanctum');
 Route::post('/vi/email/verification/confirm', [UserController::class, 'verifyEmail'])->middleware('auth:sanctum');
+
+
+
+Route::get('/v1/Explorer_courses', [CourseController::class, 'getExplorerCourses']);
