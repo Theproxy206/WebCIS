c2868fe662ab86aded487668d91dc7a90f59319f
Sergio Emanuel Soberano Paredes <soberanoparedes@proton.me>
2026-07-17 16:35:25 -0600

    created Medals model

---
+++ app/Models/Medal.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Course extends Model
+{
+    protected $table = 'medals'
+    protected $primaryKey = 'med_serial';
+    public $incrementing = true;
+    protected $keyType = 'unsignedInteger';
+
+    protected $fillable = [
+        'med_name',
+        'med_description',
+        'med_path_image',
+    ];
+
+    public function lessons(): BelongsToMany
+    {
+        return $this->hasMany(User::class, 'users_medals', 'fk_medals', 'fk_users', 'med_serial', 'user_id')->withPivot('obtained_at');
+    }
+}
