a2646843157d1523a32fc1acac5f85d73fc58638
Sergio Emanuel Soberano Paredes <soberanoparedes@proton.me>
2026-07-23 17:44:45 -0600

    reworked courses_users table

--- database/migrations/2026_01_13_213119_create_pivots.php
+++ database/migrations/2026_01_13_213119_create_pivots.php
@@ -26,7 +26,11 @@ return new class extends Migration
             $table->foreignUuid('fk_users')->references('user_id')->on('users');
             $table->string('fk_courses', 12);
 
-            $table->char('type', 3);
+            $table->unsignedTinyInteger('role');
+            $table->unsignedTinyInteger('status');
+            $table->timestampTz('joined_at')->useCurrent();
+            $table->timestampTz('completed_at')->nullable();
+            $table->timestampTz('last_accessed_at')->nullable();
 
             $table->primary(['fk_courses', 'fk_users']);
 
