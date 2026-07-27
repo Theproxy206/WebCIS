f4672c0b145119083238dac850ff55190a340b39
Sergio Emanuel Soberano Paredes <soberanoparedes@proton.me>
2026-07-27 02:42:39 -0600

    added users relation to lessons

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Lesson extends Model
 {
@@ -28,5 +29,11 @@ class Lesson extends Model
     public function subLessons(): HasMany
     {
         return $this->hasMany(Lesson::class, 'fk_lessons_lessons', 'les_serial');
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'users_lessons', 'fk_lessons', 'fk_users', 'les_serial', 'user_id')
+        ->withPivot('completed');
     }
 }
