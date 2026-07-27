bfe4d846278487c523917631874e570b64d28378
Sergio Emanuel Soberano Paredes <soberanoparedes@proton.me>
2026-07-27 02:43:46 -0600

    refactored dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,23 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use App\Http\Services\MedalService;
+use App\Http\Services\EnrollmentService;
 
 class DashboardController extends Controller
 {
+    public function __construct(
+        private readonly MedalService $medalService,
+        private readonly EnrollmentService $enrollments
+    ) {}
+
     public function index(Request $request) {
-        //TODO:
-        // Regresar datos basicos del usuario: Nombre, imagen de perfil y rol
-        // Devolver ultimos 5 cursos y ultimo curso accedido
-        // Devolver numero de medallas obtenidas
-        // Devolver porcentaje de avance global medido por lecciones completadas / lecciones totales
-
-        $user = $request->user();
-        $username = $user->user_name;
-        $userRole = $user->user_type;
-
         return response()->json([
-            'username' => $username,
-            'user_role' => $userRole
-        ], 200);
+            'user' => new UserResource($request->user()),
+            'medals' => $this->medalService->count($request->user()),
+            'progress' => $this->enrollments->progress($request->user()),
+            'recent_courses' => $this->enrollments->recentCourses($request->user())->map(function ($course) {
+                return [
+                    'token' => $course->course_token,
+                    'title' => $course->cou_title,
+                    'short_title' => $course->cou_short_title,
+                    'icon' => $course->cou_path_icon,
+                    'last_accessed_at' => $course->enrollment->last_accessed_at,
+                ];
+            }),
+        ]);
     }
 }
