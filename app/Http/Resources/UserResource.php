cbbbab5a738762bef03855897ff9361303c69d36
Sergio Emanuel Soberano Paredes <soberanoparedes@proton.me>
2026-07-28 17:07:04 -0600

    added control number, description and banner to user profile

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,11 +17,14 @@ class UserResource extends JsonResource
         return [
             'username' => $this->user_username,
             'email' => $this->user_email,
+            'control_number' => $this->user_control_number,
             'name' => $this->user_name,
             'surname' => $this->user_surname,
             'second_surname' => $this->user_second_surname,
+            'description' => $this->user_description,
             'type' => $this->user_type->name,
             'profile' => $this->user_path_profile_picture,
+            'banner' => $this->user_path_banner,
             'created_at' => $this->created_at?->toIso8601String(),
         ];
     }
